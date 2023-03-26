22c9d1cacd2a82c7ed78acc41876ed96ad0d9b2e
MJ <63952460+0x4B4A@users.noreply.github.com>
2023-03-26 04:08:26 +0100

    Update agree.c

--- agree.c
+++ agree.c
@@ -3,9 +3,9 @@
 
 int main(void)
 {
-    char c = get_char("This should be a bool but I am doint in for learning\n> ");
+    char c = get_char("This should be a bool but I am doing it in for learning\n> ");
 
-    if (c=='y')
+    if (c=='y' || c=='Y')
     {
         printf("Agree.\n");
     }
